b69e265988d3b9b18569751c078e8fb5e4d2abde
Alexandre Zelante <alexandre.zelante@outlook.com>
2019-08-05 22:42:05 -0300

    filtering added

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,18 +1,22 @@
 import React, { Component } from "react";
 import { getMovies } from "../services/fakeMovieService";
+import { getGenres } from "../services/fakeGenreService";
 import Movie from "./movie";
 import Pagination from "./common/pagination";
 import { paginate } from "../utils/paginate";
+import ListGroup from "./common/listGroup";
 
 class Movies extends Component {
     state = {
         movies: [],
         pageSize: 4,
-        currentPage: 1
+        currentPage: 1,
+        genres: []
     };
 
     componentDidMount() {
-        this.setState({ movies: getMovies() });
+        const genres = [{ name: "All genres" }, ...getGenres()];
+        this.setState({ movies: getMovies(), genres });
     }
 
     handleDelete = movie => {
@@ -33,53 +37,78 @@ class Movies extends Component {
         this.setState({ movies });
     };
 
+    handleGenreSelect = genre => {
+        this.setState({ selectedGenre: genre, currentPage: 1 });
+    };
+
     render() {
-        const { movies: allMovies, pageSize, currentPage } = this.state;
+        const {
+            movies: allMovies,
+            pageSize,
+            currentPage,
+            selectedGenre
+        } = this.state;
 
         if (allMovies.length === 0) {
             return <h1>No movies available</h1>;
         } else {
-            const movies = paginate(allMovies, currentPage, pageSize);
+            const filtered =
+                selectedGenre && selectedGenre._id
+                    ? allMovies.filter(m => m.genre._id === selectedGenre._id)
+                    : allMovies;
+            const movies = paginate(filtered, currentPage, pageSize);
+
             return (
-                <div>
-                    <h1>Showing {allMovies.length} movies in the database</h1>
-                    <table className="table">
-                        <thead>
-                            <tr>
-                                <th>Title</th>
-                                <th>Genre</th>
-                                <th>Stock</th>
-                                <th>Rate</th>
-                                <th />
-                                <th />
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {movies.map(movie => {
-                                return (
-                                    <Movie
-                                        key={movie._id}
-                                        _id={movie._id}
-                                        title={movie.title}
-                                        genre={movie.genre.name}
-                                        stock={movie.numberInStock}
-                                        rate={movie.dailyRentalRate}
-                                        onDelete={this.handleDelete}
-                                        liked={movie.liked}
-                                        handleLike={() =>
-                                            this.handleLike(movie)
-                                        }
-                                    />
-                                );
-                            })}
-                        </tbody>
-                    </table>
-                    <Pagination
-                        itemsCount={allMovies.length}
-                        pageSize={pageSize}
-                        currentPage={currentPage}
-                        onPageChange={this.handlePageChange}
-                    />
+                <div className="row">
+                    <div className="col-2">
+                        <ListGroup
+                            items={this.state.genres}
+                            selectedItem={this.state.selectedGenre}
+                            onItemSelect={this.handleGenreSelect}
+                        />
+                    </div>
+                    <div className="col">
+                        <h1>
+                            Showing {filtered.length} movies in the database
+                        </h1>
+                        <table className="table">
+                            <thead>
+                                <tr>
+                                    <th>Title</th>
+                                    <th>Genre</th>
+                                    <th>Stock</th>
+                                    <th>Rate</th>
+                                    <th />
+                                    <th />
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {movies.map(movie => {
+                                    return (
+                                        <Movie
+                                            key={movie._id}
+                                            _id={movie._id}
+                                            title={movie.title}
+                                            genre={movie.genre.name}
+                                            stock={movie.numberInStock}
+                                            rate={movie.dailyRentalRate}
+                                            onDelete={this.handleDelete}
+                                            liked={movie.liked}
+                                            handleLike={() =>
+                                                this.handleLike(movie)
+                                            }
+                                        />
+                                    );
+                                })}
+                            </tbody>
+                        </table>
+                        <Pagination
+                            itemsCount={filtered.length}
+                            pageSize={pageSize}
+                            currentPage={currentPage}
+                            onPageChange={this.handlePageChange}
+                        />
+                    </div>
                 </div>
             );
         }
